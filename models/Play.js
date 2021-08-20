0ca94e871ea3fc0dc231650af01cb3ec6e56f10a
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-08-20 23:44:01 +0200

    Create models for project specific data

--- models/Play.js
+++ models/Play.js
@@ -1,7 +1,12 @@
 const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
-    
+    title: { type: String, required: true },
+    description: { type: String, required: true, maxLength: 50 },
+    imageUrl: { type: String, required: true },
+    public: { type: Boolean, default: false },
+    createdAt: { type: Date, required: true },
+    usersLiked: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }]
 });
 
 module.exports = model('Play', schema);
